6bf92427b6132819b7cf896348d67958f8e63e5b
BCC <82493701+UniverseOfTomorrow@users.noreply.github.com>
2022-08-11 16:36:51 +0800

    Update main.go

--- main/main.go
+++ main/main.go
@@ -9,7 +9,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/bcc", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"message": " Hello World",
+			"message": " Hello World!!!",
 		})
 	})
 	r.Run(":8888")

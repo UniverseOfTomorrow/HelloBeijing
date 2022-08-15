8e63189cc66875504541f3d5f629cb948dd188ba
zzz <45326214+CASPER-3@users.noreply.github.com>
2022-08-15 22:24:52 +0800

    draft layout desgin

--- app/src/App.js
+++ app/src/App.js
@@ -5,10 +5,14 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu } from 'antd';
+import { Input } from 'antd';
 import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 const { Header, Content, Footer, Sider } = Layout;
+
+
 
 function getItem(label, key, icon, children) {
   return {
@@ -18,6 +22,38 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
+
+function get_Item(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const menu_items = [
+  get_Item('Navigation One', 'sub1', <MailOutlined />, [
+    get_Item('Option 1', '1'),
+    get_Item('Option 2', '2'),
+    get_Item('Option 3', '3'),
+    get_Item('Option 4', '4'),
+  ]),
+  get_Item('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    get_Item('Option 5', '5'),
+    get_Item('Option 6', '6'),
+    get_Item('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+]; // submenu keys of first level
+
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
 const items = [
   getItem('Option 1', '1', <PieChartOutlined />),
@@ -33,54 +69,85 @@ const items = [
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const [openKeys, setOpenKeys] = useState(['sub1']);
+
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+
+
+
   return (
     <Layout
       style={{
         minHeight: '100vh',
       }}
     >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <Sider collapsible collapsed={true} onCollapse={(value) => setCollapsed(value)}>
         <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
-      <Layout className="site-layout">
-        <Header
-          className="site-layout-background"
-          style={{
-            padding: 0,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
+
+      <Layout>
+
+        <Sider theme="light" collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+          <div className="logo" />
+          <Input placeholder="Basic usage" />
+          <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={menu_items} />
+
+        </Sider>
+
+        <Layout className="site-layout">
+          <Header
             className="site-layout-background"
             style={{
-              padding: 24,
-              minHeight: 360,
+              padding: 0,
+            }}
+          />
+          <Content
+            style={{
+              margin: '0 16px',
             }}
           >
-            Bill is a cat.
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
+            <Breadcrumb
+              style={{
+                margin: '16px 0',
+              }}
+            >
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+
+
       </Layout>
+
+
+
     </Layout>
   );
 };

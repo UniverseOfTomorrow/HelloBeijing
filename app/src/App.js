770a36801f9f90737ae760f108192dd1dcc3b3e0
zzz <45326214+CASPER-3@users.noreply.github.com>
2022-08-18 20:45:18 +0800

    Style Test

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,15 @@
 import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
+
   UserOutlined,
+  MessageOutlined,
+  CoffeeOutlined,
+
 } from '@ant-design/icons';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu } from 'antd';
-import { Input } from 'antd';
-import React, { useState } from 'react';
+import { Breadcrumb, Layout, Menu, Avatar, Input } from 'antd';
 import 'antd/dist/antd.css';
+
+import React, { useState } from 'react';
+
 const { Header, Content, Footer, Sider } = Layout;
 
 
@@ -23,48 +23,49 @@ function getItem(label, key, icon, children) {
   };
 }
 
-function get_Item(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
+
 
 const menu_items = [
-  get_Item('Navigation One', 'sub1', <MailOutlined />, [
-    get_Item('Option 1', '1'),
-    get_Item('Option 2', '2'),
-    get_Item('Option 3', '3'),
-    get_Item('Option 4', '4'),
-  ]),
-  get_Item('Navigation Two', 'sub2', <AppstoreOutlined />, [
-    get_Item('Option 5', '5'),
-    get_Item('Option 6', '6'),
-    get_Item('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-  ]),
-  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
-  ]),
-]; // submenu keys of first level
+  getItem('明天的宇宙', 'fri1', <Avatar
+    style={{
+      backgroundColor: 'blue',
+      verticalAlign: 'middle',
+    }}
+    size="large"
 
+  >
+    {'LMY'}
+  </Avatar>),
+  getItem('保聪聪', 'fri2', <Avatar
+    style={{
+      backgroundColor: 'purple',
+      verticalAlign: 'middle',
+    }}
+    size="large"
+
+  >
+    {'保聪聪'}
+  </Avatar>),
+  getItem('Asuka', 'fri3', <Avatar
+    style={{
+      backgroundColor: 'red',
+      verticalAlign: 'middle',
+    }}
+    size="large"
+
+  >
+    {'Asuka'}
+  </Avatar>),
+];
+
+// submenu keys of first level
 const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
+//Side panel
 const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+  getItem('消息', '1', <MessageOutlined />),
+  getItem('好友', '2', <UserOutlined />),
+  getItem('聊天室', '3', <CoffeeOutlined />),
 ];
 
 const App = () => {
@@ -90,9 +91,9 @@ const App = () => {
         minHeight: '100vh',
       }}
     >
-      <Sider collapsible collapsed={true} onCollapse={(value) => setCollapsed(value)}>
+      <Sider collapsible theme='light' collapsed={true} onCollapse={(value) => setCollapsed(value)} expandIcon='null'>
         <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
 
       <Layout>
